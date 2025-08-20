f22b36091908368bb05d207debf3e844246db3c2
hardikdk121 <hardik.d.k121@gmail.com>
2025-08-20 23:05:22 +0530

    qucik sort

--- quick_sort.c
+++ quick_sort.c
@@ -1,16 +1,59 @@
 #include <stdio.h>
-void quick_sort(int array[], int low, int high) {}
+
+int getpivot(int a[], int low, int high);
+void quicksort(int a[], int low, int high) {
+  if (low < high) {
+    int p = getpivot(a, low, high);
+    quicksort(a, low, p - 1);
+    quicksort(a, p + 1, high);
+  }
+}
+int getpivot(int a[], int low, int high) {
+  int pi = a[low];
+  int i = low + 1;
+  int j = high;
+  int temp;
+
+  while (1) {
+    while (i <= high && a[i] <= pi) {
+      i++;
+    }
+    while (a[j] > pi) {
+      j--;
+    }
+    if (i < j) {
+      temp = a[i];
+      a[i] = a[j];
+      a[j] = temp;
+    } else {
+      break;
+    }
+  }
+  temp = a[low];
+  a[low] = a[j];
+  a[j] = temp;
+  return j;
+}
+
+void printarray(int a[], int n) {
+  for (int i = 0; i < n; i++) {
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+}
+
 int main() {
-  int arr[5] = {40, 50, 10, 90, 60};
-  printf("Merge sort ");
-  printf("\narray before sorting: ");
-  for (int i = 0; i < 5; i++) {
-    printf(" %d ", arr[i]);
-  }
-  quick_sort(arr, 0, 4);
-  printf("\narray after sorting: ");
-  for (int i = 0; i < 5; i++) {
-    printf(" %d ", arr[i]);
-  }
+  int a[] = {20, 50, 40, 30, 10};
+  int size = sizeof(a) / sizeof(a[0]);
+
+  printf("Quick Sort\n");
+  printf("Before sorting:");
+  printarray(a, size);
+
+  quicksort(a, 0, size - 1);
+
+  printf("After sorting:");
+  printarray(a, size);
+
   return 0;
 }
